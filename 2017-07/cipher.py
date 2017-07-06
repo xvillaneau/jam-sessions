aa3db0200822b6867bc50da9690de054f7b16919
Xavier Villaneau <xavier.villaneau@ccur.com>
2017-07-06 19:52:12 -0400

    First utility functions

--- 2017-07/cipher.py
+++ 2017-07/cipher.py
@@ -3,6 +3,32 @@ free to use this as a start, but you can use a function based
 solution instead of a class if you prefer! """
 
 
+def normalize(message):
+    """
+    Convert any string into a no-spaces capitalized string
+    :param message: Input string or key to convert
+    :type message: str
+    :return: Normalized string or key
+    """
+    return filter(str.isalpha, message.upper())
+
+
+def tabula_recta(msg, key):
+    """
+    Get the ciphered letter from the matching key letter and message letter
+    
+    :param msg: Message letter
+    :param key: Key letter
+    :return: Encrypted letter
+    """
+    orig = ord('A')
+
+    i_m = ord(msg) - orig
+    i_k = ord(key) - orig
+
+    return chr((i_m + i_k) % 26 + orig)
+
+
 class AlphabetCipher:
     def __init__(self, keyphrase):
         """ Do initial setup with keyphrase and anything
--- 2017-07/test_cypher.py
+++ 2017-07/test_cypher.py
@@ -1,5 +1,24 @@
 
-from . import cipher
+import cipher
+
+
+def test_normalize():
+    """Test cipher.normalize"""
+    all_caps = "IAMANALLCAPSSTRING"
+    assert cipher.normalize(all_caps) == all_caps
+    assert cipher.normalize("I AM AN ALL CAPS STRING") == all_caps
+    assert cipher.normalize("I* am an all CAPS string!!!!") == all_caps
+
+
+def test_tabula_recta():
+    """Test cipher.tubula_recta"""
+    assert cipher.tabula_recta('A', 'A') == 'A'
+    assert cipher.tabula_recta('A', 'B') == 'B'
+    assert cipher.tabula_recta('B', 'A') == 'B'
+    assert cipher.tabula_recta('B', 'B') == 'C'
+    assert cipher.tabula_recta('A', 'Z') == 'Z'
+    assert cipher.tabula_recta('Z', 'A') == 'Z'
+    assert cipher.tabula_recta('Z', 'Z') == 'Y'
 
 
 def test_encoding():
